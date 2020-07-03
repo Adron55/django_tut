04300207916f252c80ab7340fd68f17502e95434
root <root@localhost.localdomain>
2020-07-03 10:48:31 -0400

    ajax post

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -32,7 +32,13 @@ ALLOWED_HOSTS = []
 #REST ADDITION #A
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    # 'DEFAULT_RENDERER_CLASSES': (
+    #     'rest_framework.renderers.JSONRenderer',
+    # ),
+    # 'DEFAULT_PARSER_CLASSES': (
+    #     'rest_framework.parsers.JSONParser',
+    # )
 }
 
 # Application definition
